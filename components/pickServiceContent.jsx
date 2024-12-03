b2d4526180aefee79d043184e1c26b68b05b9487
Jhon Michael Molina <jmmmolinathebest@gmail.com>
2024-12-04 06:17:06 +0800

    Service Modal added back buttons

--- components/pickServiceContent.jsx
+++ components/pickServiceContent.jsx
@@ -1,5 +1,7 @@
 import { Text, View } from "react-native";
 import Button from "./button";
+import { TouchableOpacity } from "react-native";
+import AntDesign from "@expo/vector-icons/AntDesign";
 
 const PickServiceContent = ({
   setPahatodModal,
@@ -10,7 +12,9 @@ const PickServiceContent = ({
   setFindingRider,
 }) => {
   return (
-    <View style={{ justifyContent: "center", alignItems: "center" }}>
+    <View
+      style={{ justifyContent: "center", alignItems: "center", width: "100%" }}
+    >
       <View
         style={{
           height: 6,
@@ -20,7 +24,41 @@ const PickServiceContent = ({
           borderRadius: 20,
         }}
       ></View>
-      <Text style={{ fontSize: 25, marginBottom: 10 }}>Pick a service</Text>
+      <View
+        style={{
+          flexDirection: "row",
+          alignItems: "center",
+          justifyContent: "flex-start",
+          width: "100%",
+        }}
+      >
+        <TouchableOpacity
+          onPress={() => {
+            setServiceModal(false);
+          }}
+          style={{
+            backgroundColor: "white",
+            shadowColor: "#000",
+            shadowOffset: {
+              width: 0,
+              height: 2,
+            },
+            shadowOpacity: 0.25,
+            shadowRadius: 3.84,
+
+            elevation: 5,
+            padding: 10,
+            borderRadius: 100,
+            marginRight: 10,
+            marginBottom: 10,
+          }}
+        >
+          <AntDesign name="arrowleft" size={24} color="black" />
+        </TouchableOpacity>
+        <Text style={{ fontSize: 25, marginBottom: 10, fontWeight: "bold" }}>
+          Pick a service
+        </Text>
+      </View>
       <Button
         width={300}
         text="Transportation Services"
